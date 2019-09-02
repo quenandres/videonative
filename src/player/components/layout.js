decdc5d53481d1ddd9d96d7ee9cec3ab3b271768
Jhonatan Mejia <quenandres@gmail.com>
2019-09-01 21:53:05 -0500

    Funciona el video, y esta desde el player mostrandose desde e layout de este.

--- src/player/components/layout.js
+++ src/player/components/layout.js
@@ -1,32 +1,46 @@
 import React from 'react';
-import { 
-    View,
-    Text,
-    StyleSheet
- } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
 
- function Layout(props) {
-    return (
-        <View style={styles.container}>
-            <View style={styles.video}>
-                {props.video}                
-            </View>
-        </View>
-    )
+function Layout(props) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.video}>
+        {props.video}
+      </View>
+      <View style={styles.overlay}>
+        {
+          props.loading &&
+          props.loader
+        }
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        paddingTop: '56.25%',
-    },
-    video: {
-        position: 'absolute',
-        left: 58,
-        top: 100,
-        left: 0,
-        bottom: 0,
-        backgroundColor: 'black',
-    }
+  container: {
+    paddingTop: '56.25%',
+  },
+  video: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
+  },
+  overlay:{
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    bottom: 0,
+    right: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 })
 
 export default Layout;
--- src/player/containers/player.js
+++ src/player/containers/player.js
@@ -1,36 +1,60 @@
 import React, { Component } from 'react';
-import { 
-    StyleSheet
- } from 'react-native';
-
 import { Video } from 'expo-av';
-import Layout from '../components/layout'
+import {
+  StyleSheet,
+  ActivityIndicator
+} from 'react-native';
+import Layout from '../components/layout';
 
-class Player extends Component {
-    render(){
-        return(
-            <Layout
-                video={
-                    <Video
-                        source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}              
-                        style={styles.video}
-                        resizeMode="contain"
-                    />
-                }
-            />
-        )
-    }   
+class Player extends Component {  
+  state = {
+    loading: true,
+  }
+  onBuffer = ({ isBuffering }) => {
+    this.setState({
+      loading: false
+    })
+  }
+  onLoad = () => {
+    this.setState({
+      loading: false
+    })
+  }
+  
+  render() {
+    return (
+      <Layout
+        loading={this.state.loading}
+        video = {
+          <Video
+            source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+            rate={1.0}
+            volume={1.0}
+            isMuted={false}
+            resizeMode="cover"
+            shouldPlay
+            isLooping
+            onBuffer={this.onBuffer}
+            isLoaded={this.onLoad}
+            style={styles.video}
+          />
+        }
+        loader={
+          <ActivityIndicator color="red"/>
+        }
+      />
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    video: {
-        backgroundColor: 'yellow',
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        bottom: 0,
-        top: 0,
-    }
+  video: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    top: 0,
+  }
 })
 
-export default Player;
+export default Player
